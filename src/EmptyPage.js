707f39184cc6bd32192dca1b4134324b31c34112
Seung-h-HEO <lutise@naver.com>
2021-12-27 21:31:14 +0900

    Revert "Revert "first commit""
    This reverts commit 04b5d964680c4564875da177dfcb6aea0e913eb4.

---
+++ src/EmptyPage.js
@@ -0,0 +1,16 @@
+import REact, { Component } from "react";
+import { render } from "react-dom";
+import { Link } from "react-router-dom";
+
+class EmptyPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>잘못된 접근입니다.</h2>
+        <Link to="/">돌아가기</Link>
+      </div>
+    );
+  }
+}
+
+export default EmptyPage;
